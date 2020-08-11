0fa9e52fb0ae55cd90e04853ebddc6ef8d74a4de
Benriez <33022072+Benriez@users.noreply.github.com>
2020-08-11 17:05:16 +0200

    add photo frame & capture button

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,6 +1,17 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img
+        class="full-width"
+        src="https://cdn.quasar.dev/img/mountains.jpg">
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        round
+        size="lg"
+        color="grey-10"
+        icon="eva-camera" />
+    </div>
   </q-page>
 </template>
 
@@ -9,3 +20,10 @@ export default {
   name: 'PageCamera'
 }
 </script>
+
+
+<style lang="sass">
+  .camera-frame
+    border: 2px solid $grey-10
+    border-radius: 10px
+</style>

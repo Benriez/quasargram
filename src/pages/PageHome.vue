6bf1e79361368081a6a139a8326ee33bad3b2d22
Benriez <33022072+Benriez@users.noreply.github.com>
2020-08-11 15:50:58 +0200

    add profile for desktop

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,33 +1,55 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card
-      v-for="post in posts"
-      :key="post.id"
-      class="card-post q-mb-md"
-      flat
-      bordered>
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-          </q-avatar>
-        </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-bold">benny_riezler</q-item-label>
-          <q-item-label caption>
-            {{post.location}}
-          </q-item-label>
-        </q-item-section>
-      </q-item>
+    <div class="row q-col-gutter-lg">
+      <div class="col-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered>
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
 
-      <q-separator />
-      <q-img :src="post.imageUrl" />
-      <q-card-section>
-        <div>{{post.caption}}</div>
-        <div class="text-caption text-grey">{{post.date | dateToday}}</div>
-      </q-card-section>
-    </q-card>
+            <q-item-section>
+              <q-item-label class="text-bold">benny_riezler</q-item-label>
+              <q-item-label caption>
+                {{post.location}}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+          <q-img :src="post.imageUrl" />
+          <q-card-section>
+            <div>{{post.caption}}</div>
+            <div class="text-caption text-grey">{{post.date | dateToday}}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">benny_riezler</q-item-label>
+            <q-item-label caption>
+              Benny
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
+
   </q-page>
 </template>
 

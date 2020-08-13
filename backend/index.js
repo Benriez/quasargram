99869904bf6a8cb5d87a26418926efeca5b4378b
Benriez <stberiez@stud.h-da.de>
2020-08-13 19:34:13 +0200

    store field data as a post

--- backend/index.js
+++ backend/index.js
@@ -47,6 +47,8 @@ app.post('/createPost', (request, response) => {
 
     var busboy = new Busboy({ headers: request.headers });
 
+    let fields = {}
+
     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
       console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
       file.on('data', function(data) {
@@ -58,12 +60,17 @@ app.post('/createPost', (request, response) => {
     });
 
     busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-      console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+      fields[fieldname] = val 
     });
 
     busboy.on('finish', function() {
-      console.log('Done parsing form!');
-      //response.writeHead(303, { Connection: 'close', Location: '/' });
+      db.collection('posts').doc(fields.id).set({
+        id: fields.id,
+        caption: fields.caption,
+        location: fields.location,
+        date: fields.date,
+        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/quasargram-5a2d4.appspot.com/o/goldenGate.jpg?alt=media&token=4440b884-9235-472b-af8c-855291178aca'
+      })
       response.send('Done parsing form');
     });
     request.pipe(busboy);

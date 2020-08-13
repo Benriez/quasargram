a3336600994352ccd713faad558944af81175566
Benriez <stberiez@stud.h-da.de>
2020-08-13 21:41:22 +0200

    fixed uuid

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@
     let path = require('path')
     let os = require('os')
     let fs = require('fs')
+    let UUID = require('uuid-v4')
 
 /*
     config express
@@ -34,6 +35,9 @@
 */
     app.get('/posts', (request, response) => {
         response.set('Access-Control-Allow-Origin', '*')
+
+        let uuid = UUID()
+
         let posts = []
         db.collection('posts').orderBy('date', 'desc').get().then(snapshot => {
             snapshot.forEach((doc) => {
@@ -52,6 +56,7 @@ app.post('/createPost', (request, response) => {
 
     var busboy = new Busboy({ headers: request.headers });
 
+    let uuid = UUID()
     let fields = {}
     let fileData = {}
 
@@ -68,14 +73,34 @@ app.post('/createPost', (request, response) => {
     });
 
     busboy.on('finish', function() {
-      db.collection('posts').doc(fields.id).set({
-        id: fields.id,
-        caption: fields.caption,
-        location: fields.location,
-        date: parseInt(fields.date),
-        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/quasargram-5a2d4.appspot.com/o/goldenGate.jpg?alt=media&token=4440b884-9235-472b-af8c-855291178aca'
-      })
-      response.send('Done parsing form');
+      bucket.upload(
+          fileData.filepath,
+          {
+              uploadType: 'media',
+              metadata: {
+                  metadata: {
+                     contentType: fileData.mimetype,
+                     firebaseStorageDownloadTokens: uuid
+                  }
+              }
+          },
+          (err, uploadedFile) => {
+              if (!err) {
+                createDocument(uploadedFile)
+              }
+          }
+      )
+      function createDocument(uploadedFile){
+        db.collection('posts').doc(fields.id).set({
+            id: fields.id,
+            caption: fields.caption,
+            location: fields.location,
+            date: parseInt(fields.date),
+            imageUrl: `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${uploadedFile.name}?alt=media&token=${uuid}`
+        }).then(() => {
+            response.send('Post added: ' + fields.id)
+        })
+      }
     });
     request.pipe(busboy);
 })
